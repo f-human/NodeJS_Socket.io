ea964957ef6b437f9e700e8d5139eefee9258d10
f-Human <functi0nal@hotmail.com>
2019-09-02 00:31:17 +0600

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # NodeJS_Socket.io
+
+A very basic usage example of Socket.io in NodeJS server & Pure HTML (static) client.
+
+## How to run?
+* clone or download the project
+* run `npm install` to install dependencies
+* run `npm start` to run the server
+* the simply double-click on index.html and try
+* open the index.html file on multiple tab or browser to test chat-ability
---
+++ server.js
@@ -0,0 +1,23 @@
+const PORT = 3000;
+const server = require('http').createServer();
+
+const io = require('socket.io')(server, {
+    path: '/ChatHub'
+});
+
+io.on('connection', socket => {
+    const query = socket.handshake.query;
+    const user = query.user;
+
+    console.log(`User ${user} conntected to the ChatHub`);
+    io.emit('new user', { user });
+
+    socket.on('chat message', function (msg) {
+        console.log(`new message: ${msg}`);
+        io.emit('chat message', msg);
+    });
+});
+
+server.listen(PORT, _ => {
+    console.log(`Voila! Head over http://localhost:${PORT}`);
+});
